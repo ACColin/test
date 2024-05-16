50886061f27f447082279dbd690f0da01095e5db
Anne-Cecile Colin <60205617+ACColin@users.noreply.github.com>
2024-05-16 10:15:27 +1000

    all

---
+++ scr/1_first_script.R
@@ -0,0 +1,9 @@
+print("Hello World")
+
+headers = c("A", "B", "C", "D", "E")
+random_data = matrix(data = round(runif(20, min = 1, max = 100)), nrow = 4, ncol = 5)
+df = as.data.frame(random_data)
+colnames(df) = headers
+print(df)
+
+write.csv(df, "outputs/table_output.csv")
